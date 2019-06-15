20fcd7a73496e915925fed9dd0f45251e3f1d4b3
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-06-15 01:06:32 -0300

    Categorias e Tags

--- wp-content/themes/tema1/sidebar.php
+++ wp-content/themes/tema1/sidebar.php
@@ -1,1 +1,27 @@
-<p>Side Bar 1</p>
+<?php //get_search_form() ?>
+<h3>Categorias</h3>
+<ul class="list-group">
+    <?php
+        $categories = get_categories();
+        foreach($categories as $category):
+            printf('<li class="list-group-item"><a href="%s" title="%s">%s</a></li>',
+                get_category_link($category->term_id),
+                sprintf("Ver post de %s", $category->name),
+                $category->name);
+        endforeach;
+    ?>
+</ul>
+<br/>
+
+<h3>Tags</h3>
+<ul class="list-group">
+    <?php
+    $tags = get_tags();
+    foreach($tags as $tag):
+        printf('<li class="list-group-item"><a href="%s" title="%s">%s</a></li>',
+            get_tag_link($tag->term_id),
+            sprintf("Ver post sobre %s", $tag->name),
+            $tag->name);
+    endforeach;
+    ?>
+</ul>
